4f7f3a41d359b0603804a54a9ff49b618031ed79
Anindita Mazumder <anindita.mazumder1@t-mobile.com>
2023-01-24 23:07:40 +0530

    classpath direct

--- src/main/java/com/example/demo/config/DemoConfig.java
+++ src/main/java/com/example/demo/config/DemoConfig.java
@@ -1,18 +1,18 @@
-//package com.example.demo.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
-//import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-//import org.springframework.core.io.ClassPathResource;
-//
-//@Configuration
-//@PropertySource("classpath:./git.properties")
-//public class DemoConfig {
-//	@Value("${git.commit.id}")
-//	private String commitId;
-//
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@PropertySource({"git.properties"})
+public class DemoConfig {
+	@Value("${git.commit.id}")
+	private String commitId;
+
 //	@Bean
 //	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
 //		PropertySourcesPlaceholderConfigurer propsConfig = new PropertySourcesPlaceholderConfigurer();
@@ -21,4 +21,4 @@
 //		propsConfig.setIgnoreUnresolvablePlaceholders(true);
 //		return propsConfig;
 //	}
-//}
+}
