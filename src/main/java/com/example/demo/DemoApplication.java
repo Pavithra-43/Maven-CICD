9ed6d28904a267ed6fedc2b2f637c1bd7bdfb929
Anindita Mazumder <anindita.mazumder1@t-mobile.com>
2023-01-24 22:45:50 +0530

    commit id in main class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +19,29 @@ public class DemoApplication {
 		return "Hey there! Welcome..";
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Java Upgrade to 17");
 
+		Properties prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		InputStream stream = loader.getResourceAsStream("git.properties");
+		if (stream != null) {
+			prop.load(stream);
+			String id = prop.getProperty("git.commit.id");
+//             String dirty = prop.getProperty("git.dirty");
+//             if (dirty.equals("false")) {
+//                 dirty = "";
+//             } else {
+//                 dirty = "(dirty)";
+//             }
+//             ver = ver + prop.getProperty("git.build.version")+dirty;
+//             ver = ver + " - " + prop.getProperty("git.commit.id.describe");
+//             ver = ver + " - " + prop.getProperty("git.build.time");
+			System.out.println("Commitid = " + id);
+		} else {
+			// throw new MojoFailureException("Unable to find MyProject git.properties");
+		}
 	}
 
-	
 }
